144b86b88575a120b23784fb79f6aacf80030efc
Jasmine <jasminecharles@hotmail.com>
2020-04-24 11:02:00 -0700

    names not working yet

--- names/names.py
+++ names/names.py
@@ -1,139 +1,71 @@
 import time
-# from doubly_linked_list import DoublyLinkedList
-class ListNode:
-    def __init__(self, value, prev=None, next=None):
+
+class BinarySearchTree:
+    def __init__(self, value):
         self.value = value
-        self.prev = prev
-        self.next = next
+        self.left = None
+        self.right = None
+        
 
-    """Wrap the given value in a ListNode and insert it
-    after this node. Note that this node could already
-    have a next node it is point to."""
-    def insert_after(self, value):
-        current_next = self.next
-        self.next = ListNode(value, self, current_next)
-        if current_next:
-            current_next.prev = self.next
+    # Insert the given value into the tree
+    def insert(self, value):
+        
+        node = self
+         
+        if value < node.value:
+              if node.left is None:
+                  self.left = BinarySearchTree(value)
+              else:
+                  node.left.insert(value)
 
-    """Wrap the given value in a ListNode and insert it
-    before this node. Note that this node could already
-    have a previous node it is point to."""
-    def insert_before(self, value):
-        current_prev = self.prev
-        self.prev = ListNode(value, current_prev, self)
-        if current_prev:
-            current_prev.next = self.prev
-
-    """Rearranges this ListNode's previous and next pointers
-    accordingly, effectively deleting this ListNode."""
-    def delete(self):
-        if self.prev:
-            self.prev.next = self.next
-        if self.next:
-            self.next.prev = self.prev
-
-
-"""Our doubly-linked list class. It holds references to
-the list's head and tail nodes."""
-
-
-class DoublyLinkedList:
-    def __init__(self, node=None):
-        self.head = node
-        self.tail = node
-        self.length = 1 if node is not None else 0
-
-    def __len__(self):
-        return self.length
-
-    """Wraps the given value in a ListNode and inserts it 
-    as the new head of the list. Don't forget to handle 
-    the old head node's previous pointer accordingly."""
-    def add_to_head(self, value):
-        new_node = ListNode(value, None, None)
-        self.length += 1
-        if not self.head and not self.tail:
-            self.head = new_node
-            self.tail = new_node
         else:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
+            if node.right is None:
+                node.right = BinarySearchTree(value)
+            else:
+                node.right.insert(value)
 
-    """Removes the List's current head node, making the
-    current head's next node the new head of the List.
-    Returns the value of the removed Node."""
-    def remove_from_head(self):
-        value = self.head.value
-        self.delete(self.head)
-        return value
+       
 
-    """Wraps the given value in a ListNode and inserts it 
-    as the new tail of the list. Don't forget to handle 
-    the old tail node's next pointer accordingly."""
-    def add_to_tail(self, value):
-        new_node = ListNode(value, None, None)
-        self.length += 1
-        if not self.head and not self.tail:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.prev = self.tail
-            self.tail.next = new_node
-            self.tail = new_node
 
-    """Removes the List's current tail node, making the 
-    current tail's previous node the new tail of the List.
-    Returns the value of the removed Node."""
-    def remove_from_tail(self):
-        value = self.tail.value
-        self.delete(self.tail)
-        return value
+    
+    
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target < self.value:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
+        elif target > self.value:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
 
-    """Removes the input node from its current spot in the 
-    List and inserts it as the new head node of the List."""
-    def move_to_front(self, node):
-        if node is self.head:
-            return
-        value = node.value
-        self.delete(node)
-        self.add_to_head(value)
-
-    """Removes the input node from its current spot in the 
-    List and inserts it as the new tail node of the List."""
-    def move_to_end(self, node):
-        if node is self.tail:
-            return
-        value = node.value
-        self.delete(node)
-        self.add_to_tail(value)
-
-    """Removes a node from the list and handles cases where
-    the node was the head or the tail"""
-    def delete(self, node):
-        self.length -= 1
-        if self.head is self.tail:
-            self.head = None
-            self.tail = None
-        elif self.head is node:
-            self.head = node.next
-            node.delete()
-        elif self.tail is node:
-            self.tail = node.prev
-            node.delete()
-        else:
-            node.delete()
-
-    """Returns the highest value currently in the list"""
+    # Return the maximum value found in the tree
     def get_max(self):
-        max_value = self.head.value
-        current = self.head
-        while current is not None:
-            if current.value > max_value:
-                max_value = current.value
-            current = current.next
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
 
-        return max_value
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
+    def for_each(self, cb):
+        cb(self.value)
+        if self.right:
+            self.right.for_each(cb)
+        
+        if self.left:
+            self.left.for_each(cb)
+
+
+    
+    
+        
 
 
 start_time = time.time()
@@ -149,18 +81,8 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-merged = names_1 + names_2
-merged.sort()
-length =len(merged)
-middle = length // 2
+tree = BinarySearchTree(value)
 
-first_half = 
-# i = 0
-# j = 0
-#      if i 
-for i in merged:
-        if merged[name] == merged:
-            duplicates.append(name)
 
 
 
